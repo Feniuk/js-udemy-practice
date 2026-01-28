15f402581aade49fa4f7681bc97013f9307f13e6
Feniuk <143640539+Feniuk@users.noreply.github.com>
2026-01-28 18:37:47 +0100

    some stuff

--- js-udemy-practice/assets/scripts/app.js
+++ js-udemy-practice/assets/scripts/app.js
@@ -1,1 +1,9 @@
-alert("Zdarova Fen!");
+let currentResult = 0;
+let operationDescription = "Mishanya skladivayet dva chisla yepta!";
+
+function add() {
+  currentResult += userInput.value;
+  outputResult(currentResult, operationDescription);
+}
+
+addBtn.addEventListener("click", add);
