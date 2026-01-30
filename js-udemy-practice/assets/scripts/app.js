c8ac7bad5673657945cf9b8b9f999a1f39cea198
Feniuk <143640539+Feniuk@users.noreply.github.com>
2026-01-30 21:25:21 +0100

    calculator logic done

--- js-udemy-practice/assets/scripts/app.js
+++ js-udemy-practice/assets/scripts/app.js
@@ -1,9 +1,43 @@
 let currentResult = 0;
-let operationDescription = "Mishanya skladivayet dva chisla yepta!";
 
-function add() {
-  currentResult += userInput.value;
+function getUserInput() {
+  return parseInt(userInput.value);
+}
+
+function outputInit(operator, resultBeforeCalc, resultAfterCalc) {
+  const operationDescription = `${resultBeforeCalc} ${operator} ${resultAfterCalc}`;
   outputResult(currentResult, operationDescription);
 }
 
+function add() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult += enteredNumber;
+  outputInit("+", initialResult, enteredNumber);
+}
+
+function subtract() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult -= enteredNumber;
+  outputInit("-", initialResult, enteredNumber);
+}
+
+function multiply() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult *= enteredNumber;
+  outputInit("*", initialResult, enteredNumber);
+}
+
+function divide() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult /= enteredNumber;
+  outputInit("/", initialResult, enteredNumber);
+}
+
 addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
