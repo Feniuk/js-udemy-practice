247f7329bfb6d41de3509f4294058ebd5160aefc
Feniuk <143640539+Feniuk@users.noreply.github.com>
2026-02-04 23:18:49 +0100

    small update

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,2 +1,14 @@
+const ATTACK_VALUE = 10;
+
 let chosenMaxHealth = 100;
-const attackValue = 10;
+let currentMonsterHealth = chosenMaxHealth;
+let currentPlayerHealth = chosenMaxHealth;
+
+adjustHealthBars(chosenMaxHealth);
+
+function attackMonster() {
+  const damage = dealMonsterDamage(ATTACK_VALUE);
+  currentMonsterHealth -= damage;
+}
+
+attackBtn.addEventListener("click", attackMonster);
